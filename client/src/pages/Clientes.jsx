8c458166f2c31b0bc4d46f6a5d4efad7d4169708
Thomas Restrepo <103452035+thomasrpo03@users.noreply.github.com>
2023-05-10 22:11:17 -0500

    Update Clientes.jsx

--- client/src/pages/Clientes.jsx
+++ client/src/pages/Clientes.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect,  useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Table, Button } from "reactstrap";
 import { NavLink } from "react-router-dom";
 import axios from "axios";
